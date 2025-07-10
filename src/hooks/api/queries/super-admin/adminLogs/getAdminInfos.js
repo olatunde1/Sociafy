3f929271298dcb5fe734288bef2ba5ffdda33bcc
dexie14 <adeluadeoniye14@gmail.com>
2025-07-10 12:37:43 +0100

    feat: single user

--- src/hooks/api/queries/super-admin/adminLogs/getAdminInfos.js
+++ src/hooks/api/queries/super-admin/adminLogs/getAdminInfos.js
@@ -4,6 +4,7 @@ import axiosInstance from "@/hooks/axiosInstace";
 export const QUERY_KEY_ALLORDERS = "getALLOrders";
 export const QUERY_KEY_Overview = "getOverview";
 export const QUERY_KEY_Users = "getUsers";
+export const QUERY_KEY_SingleUser = "getSingleUser";
 
 const GetOrders = async (params = {}) => {
   const response = await axiosInstance.get(`/admin/orders`, {
@@ -22,6 +23,13 @@ const GetOverview = async (params = {}) => {
 };
 const GetUsers = async (params = {}) => {
   const response = await axiosInstance.get(`/admin/users`, {
+    params,
+  });
+
+  return response.data;
+};
+const GetSingleUser = async (id, params = {}) => {
+  const response = await axiosInstance.get(`/admin/users/${id}`, {
     params,
   });
 
@@ -49,5 +57,12 @@ const getAdminUsers = (params) => {
     staleTime: 100,
   });
 };
+const getAdminSingleUser = (id, params) => {
+  return useQuery({
+    queryKey: [QUERY_KEY_SingleUser, id, params],
+    queryFn: () => GetSingleUser(id, params),
+    staleTime: 100,
+  });
+};
 
-export { getAdminOrders, getAdminOverview, getAdminUsers };
+export { getAdminOrders, getAdminOverview, getAdminUsers, getAdminSingleUser };
